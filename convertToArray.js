5077f069f6b880c8068e3fb2f39257fd5710b28f
vibguldev <gulativibhav@gmail.com>
2017-06-19 22:48:55 +0530

    added escape character

--- convertToArray.js
+++ convertToArray.js
@@ -6,7 +6,7 @@ const converter = (obj) => {
 }
 
 function convert2Csv(firebaseData) {
-    const fields = ['username', 'company', 'What is the company’s business model', 'What truly inspired you in this session', 'What ideas can we take back and apply at Unilever']
+    const fields = ['username', 'company', 'What is the company\'s business model', 'What truly inspired you in this session', 'What ideas can we take back and apply at Unilever']
     let jsonData = converter(firebaseData)
     return json2csv({ data: jsonData, fields: fields })
 }
